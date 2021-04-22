1d0175e77a7f4e3188bb448cacca9ed9e5179f57
John Minns <john.minns@missguided.com>
2021-04-23 00:20:09 +0100

    Adding Babel to allow Jest to do TS coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
+  collectCoverageFrom: [
+    './src/**/*',
+    '!**/*.d.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
   coveragePathIgnorePatterns: ['/MockData/', '/Test/'],
 };
